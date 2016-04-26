3c8d76f782ebb8d624e42cfa7da9f97bf8f716f9
Rifqi Maula Iqbal <rifqimaulaiqbal@gmail.com>
2016-04-26 12:32:51 +0700

    Update Write.java

--- InsecureChat/KIJ-Chat-Client/Write.java
+++ InsecureChat/KIJ-Chat-Client/Write.java
@@ -9,6 +9,20 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.security.MessageDigest;
+import java.util.Arrays;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+import java.io.BufferedReader; 
+import java.io.InputStreamReader; 
+import java.io.IOException; 
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  *
@@ -20,6 +34,9 @@ public class Write implements Runnable {
         private PrintWriter out;
         boolean keepGoing = true;
         ArrayList<String> log;
+        static String IV = "AAAAAAAAAAAAAAAA";
+        //static String plaintext ; /*Note null padding*/
+        static String encryptionKey = "0123456789abcdef";
 	
 	public Write(Scanner chat, PrintWriter out, ArrayList<String> log)
 	{
@@ -36,6 +53,9 @@ public class Write implements Runnable {
 			while (keepGoing)//WHILE THE PROGRAM IS RUNNING
 			{						
 				String input = chat.nextLine();	//SET NEW VARIABLE input TO THE VALUE OF WHAT THE CLIENT TYPED IN
+                                char[] a= input.toCharArray();
+                                //System.out.println(a[1]);
+                                
 				out.println(input);//SEND IT TO THE SERVER
 				out.flush();//FLUSH THE STREAM
                                 
@@ -51,5 +71,11 @@ public class Write implements Runnable {
 			e.printStackTrace();//MOST LIKELY WONT BE AN ERROR, GOOD PRACTICE TO CATCH THOUGH
 		} 
 	}
+    public static byte[] encrypt(String plainText, String encryptionKey) throws Exception {
+    Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding", "SunJCE");
+    SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
+    cipher.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+    return cipher.doFinal(plainText.getBytes("UTF-8"));
+  }
 
 }
